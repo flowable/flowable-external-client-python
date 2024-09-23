40b4c533a6bef91dfef71e0f0e1b946d4621b1b0
Valentin Zickner <valentin.zickner@flowable.com>
2024-09-23 17:59:02 +0200

    change version to 1.0.1rc2

--- robocorp/setup.py
+++ robocorp/setup.py
@@ -9,14 +9,14 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='flowable.robocorp-client',
     packages=['flowable', 'flowable.robocorp_client'],
-    version='1.0.1rc1',
+    version='1.0.1rc2',
     description='Flowable client to be used with Robocorp. This client connects to a Flowable instance via external worker and executes robocorp tasks or actions.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Flowable',
     license='Apache License, Version 2.0',
-    install_requires=['flowable.external-worker-client>=1.0.1rc1', 'robocorp-actions>=0.2.1', 'robocorp-tasks>=3.1.1', 'robocorp-truststore>=0.9.1'],
+    install_requires=['flowable.external-worker-client>=1.0.1rc2', 'robocorp-actions>=0.2.1', 'robocorp-tasks>=3.1.1', 'robocorp-truststore>=0.9.1'],
     extras_require={
-        'testing': ['pytest', 'vcrpy', 'flowable.external-worker-client>=1.0.1rc1']
+        'testing': ['pytest', 'vcrpy', 'flowable.external-worker-client>=1.0.1rc2']
     },
 )
